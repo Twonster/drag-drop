9f3f9e0eeced2b6da13cccbe2aad0226c716d43f
Anton Leybov <leybov.anton@gmail.com>
2021-05-02 13:08:08 +0300

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.getElementsByClassName('text-inp')[0]
 const button = document.getElementsByClassName('confirm-btn')[0]
 const container = document.getElementsByClassName('words-container')[0]
+const clone_container = document.getElementsByClassName('container')[0]
 
 function spawnItem(val, id) {
     const item = document.createElement('div')
@@ -20,38 +21,55 @@ function createWord() {
         const { left, top } = item.getBoundingClientRect()
         console.log(left)
         item.style.left = left + 'px'
-        item.style.top = top
+        item.style.top = top + 'px'
     })
+    spawnedItem.forEach(item => item.style.position = 'absolute')     
 }
 
 function takeLetter(event) {
     if (event.target.className === 'item') {
         console.log('take')
         const element = document.getElementById(event.target.id)
-        element.style = `transform: scale(1.1); position: absolute; z-index: 10; left: ${event.pageX - element.offsetWidth / 2}px; top: ${event.pageY - element.offsetWidth / 2}px`
-        
+        const clone = document.createElement('div')
+        const initPosition = {
+            val: `${element.innerText}`,
+            left: `${event.pageX - element.offsetWidth / 2}`,
+            top: `${event.pageY - element.offsetWidth / 2}`,
+            leftPosition: `${event.target.style.left}`,
+            rightPosition: `${event.target.style.top}`
+        }
+
+        clone.classList.add('clone')
+        clone.innerText = element.innerText
+        clone.style = `transform: scale(1.1); position: absolute; z-index: 10; left: ${initPosition.left}px; top: ${initPosition.top}px`
+        clone_container.append(clone)
         
         function drag (event) {
             console.log('drag')
-            element.style.left = `${ event.pageX - element.offsetWidth / 2 }px`
-            element.style.top =`${event.pageY - element.offsetWidth / 2 }px`
+            clone.style.left = `${ event.pageX - clone.offsetWidth / 2 }px`
+            clone.style.top =`${ event.pageY - clone.offsetWidth / 2 }px`
         }
         
         function pushLetter(event) {
             console.log('push')
-            const element = document.getElementById(event.target.id)
-            element.style = `transform: none; position: absolute; left: ${event.pageX - element.offsetWidth / 2}px; top: ${event.pageY - element.offsetWidth / 2}px` 
-            element.removeEventListener('mousemove', drag)
-            element.removeEventListener('mouseup', pushLetter)
-            element.removeEventListener('mousedown', takeLetter)
+            clone_container.removeChild(clone)
+            const mouse = document.elementFromPoint(event.clientX, event.clientY)
+            if (mouse.className === 'item') {
+                element.style = `transform: none; position: absolute; left: ${mouse.style.left}; top: ${mouse.style.top}` 
+                mouse.style = `transform: none; position: absolute; left: ${initPosition.leftPosition}; top: ${initPosition.rightPosition}`
+            } else {
+                element.style = `transform: none; position: absolute; left: ${event.pageX - element.offsetWidth / 2}px; top: ${event.pageY - element.offsetWidth / 2}px` 
+            }
+            clone.removeEventListener('mousemove', drag)
+            clone.removeEventListener('mouseup', pushLetter)
+            clone.removeEventListener('mousedown', takeLetter)
         }
         
-        element.addEventListener('mouseup', pushLetter)
-        element.addEventListener('mousemove', drag)
-        element.ondragstart = () => false;
+        clone.addEventListener('mouseup', pushLetter)
+        clone.addEventListener('mousemove', drag)
+        clone.ondragstart = () => false;
     }
 }
-
 
 button.addEventListener('click', createWord)
 container.addEventListener('mousedown', takeLetter)
